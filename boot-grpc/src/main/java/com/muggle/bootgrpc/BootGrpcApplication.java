806a02c2f8677d312101f69504ad482a2bb3e288
muggle <1977339740@qq.com>
2019-05-14 13:52:12 +0800

    test

---
+++ boot-grpc/src/main/java/com/muggle/bootgrpc/BootGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.muggle.bootgrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootGrpcApplication.class, args);
+    }
+
+}
