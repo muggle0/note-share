ba048b1202562113ee876b91a4ecf70d3a774fe6
muggle <1977339740@qq.com>
2019-05-14 17:46:52 +0800

    实验失败

---
+++ boot-client/src/main/java/com/muggle/bootclient/BootClientApplication.java
@@ -0,0 +1,13 @@
+package com.muggle.bootclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootClientApplication.class, args);
+    }
+
+}
